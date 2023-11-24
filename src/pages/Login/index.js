7dcfb96e98506fda9807dc80ef03c7454cc0a325
vuongthai1205 <2051010367vuong@ou.edu.vn>
2023-11-24 18:14:55 +0700

    last commit

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -5,15 +5,15 @@ import cookie from "react-cookies";
 import Form from "react-bootstrap/Form";
 import { MyUserContext } from "../../App";
 import apiConfig, { authApi, endpoints } from "../../config/apiConfig";
-import { Col, Container, Row } from "react-bootstrap";
+import { Col, Container, Row, Spinner } from "react-bootstrap";
 function Login() {
   const [user, dispatch] = useContext(MyUserContext);
-
+  const [loading, setLoading] = useState(false);
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const navigate = useNavigate();
   const [validated, setValidated] = useState(false);
-  const [error, setError] = useState('');
+  const [error, setError] = useState("");
   const handleLogin = async (e) => {
     e.preventDefault();
     const form = e.currentTarget;
@@ -23,31 +23,31 @@ function Login() {
       setValidated(true); // Set validated to true to display validation messages
       return;
     }
+    setLoading(true);
     try {
       const response = await apiConfig.post(endpoints["login"], {
         username,
         password,
       });
-      if(response.status === 200) {
+      if (response.status === 200) {
+        setLoading(false);
+
         cookie.save("token", response.data.token);
         let { data } = await authApi().get(endpoints["current-user"]);
         cookie.save("user", data);
-  
+
         dispatch({
           type: "login",
           payload: data,
         });
+      } else if (response.status === 400) {
+        setError("Sai mật khẩu");
       }
-      else if (response.status === 400){
-        setError("Sai mật khẩu")
-      }
-      
     } catch (error) {
-      if(error.response.status === 400){
-        setError("Sai mật khẩu")
-      }
-      else{
-        setError("Kiểm tra lại thông tin")
+      if (error.response.status === 400) {
+        setError("Sai mật khẩu");
+      } else {
+        setError("Kiểm tra lại thông tin");
       }
     }
   };
@@ -59,7 +59,9 @@ function Login() {
       <Container>
         <Row>
           <Col>
-          <h2 className="font-['Calistoga'] text-[25px] text-center">Trang đăng nhập</h2>
+            <h2 className="font-['Calistoga'] text-[25px] text-center">
+              Trang đăng nhập
+            </h2>
             <Form noValidate validated={validated} onSubmit={handleLogin}>
               <Form.Group className="mb-3" controlId="formBasicEmail">
                 <Form.Label>Tên đăng nhập</Form.Label>
@@ -85,8 +87,9 @@ function Login() {
               <Button
                 className="bg-color-btn-main relative"
                 type="submit"
-                variant="primary">
-                Đăng nhập 
+                variant="primary"
+                disabled={loading}>
+                {!loading ? "Đăng nhập" : <Spinner />}
               </Button>
               <span className="text-color-btn-danger ml-[12px]">{error}</span>
             </Form>
